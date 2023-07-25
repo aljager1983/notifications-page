a360dc36019a0dcea6f4db6d07a17b323a0cdeab
promdiGamer <al_jager1983@yahoo.com>
2023-07-25 22:15:39 +0800

    7-finalized the js for read/unread function

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const unreadID = document.getElementById("post7");
 const markRead = document.getElementById("read");
 const dotUnread = document.getElementsByClassName("dotUnread");
 const notiCount = document.getElementById("notiCount");
+const unreadArr = Array.from(unreads);
+const dots = Array.from(dotUnread);
+
 let i = 0;
 let counts = 3;
 markRead.addEventListener("click", function() {
@@ -15,21 +18,23 @@ markRead.addEventListener("click", function() {
     
 })
 
-for(let x of unreads) {
-    x.addEventListener("click", function() {
-        
-        this.classList = "read";
+
+
+
+unreadArr.forEach( (button, index) => 
+{
+    button.addEventListener("click", () => 
+    {
+        dotUnread[index].style.display = "none";
+        unreads[index].style.backgroundColor = "white";
         counts -= 1;
         notiCount.innerHTML = counts;
         if(counts < 0) {
             notiCount.innerHTML = 0;
         }
-        
-        console.log(unreads);
-        // console.log(this.children)
-        
-    })
-}
+
+    });
+})
 
 function size () {
     let h = window.innerHeight;
